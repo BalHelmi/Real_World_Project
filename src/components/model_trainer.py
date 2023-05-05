c77a79dcc0ea7c45248d2c113e8be4a24fa016d0
BalHelmi <balhoudihelmi@gmail.com>
2023-05-05 13:47:19 +0100

    Update

--- src/components/model_trainer.py
+++ src/components/model_trainer.py
@@ -42,7 +42,6 @@ class ModelTrainer():
             
             model_report:dict=evaluate_models(X_train=X_train,y_train=y_train,X_test=X_test,y_test=y_test,models=models)
              
-            # find the best score model 
             best_model_score = max(sorted(model_report.values()))
             best_model_name = list(model_report.keys())[list(model_report.values()).index(best_model_score)]
             best_model = models[best_model_name]
